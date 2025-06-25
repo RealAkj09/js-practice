09f17712bb3aca4b9ced97080993444289cc96c7
RealAkj09 <ajkrisgreat@gmail.com>
2025-06-25 06:19:48 +0000

    datatypes of js

---
+++ basics/dt.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) we are using node js not browser
+
+console.log(3+
+    3
+);// code redeability should be high
+
+console.log("Ayush");
+
+let name = "Ayush" //string
+let age = 23 //number
+let isLoggedIn = false //boolean
+let state; //undefined
+
+console.log(typeof age); 
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+
+
+
+
