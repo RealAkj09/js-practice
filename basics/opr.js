69d6429730e40ac24aca452dce6f2c202c6a43a1
RealAkj09 <ajkrisgreat@gmail.com>
2025-06-26 06:43:52 +0000

    Operations

--- basics/opr.js
+++ basics/opr.js
@@ -20,12 +20,18 @@ console.log(1+2+"2")
 
 /* String first then all considered
  into string otherwise whatever is first */
+console.log(true+1);
+console.log(+"");let num1,num2,num3
+num1=num2=num3= 2 + 2
 
- console.log(true+1);
- console.log(+"");
- 
- let num1,num2,num3
+let x = 3;
+const y = x++;
 
- num1=num2=num3= 2 + 2
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
 
- 
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
