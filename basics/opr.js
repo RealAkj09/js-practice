0a251c0566e3ac33c5d76456366fe1bc99881f1c
RealAkj09 <ajkrisgreat@gmail.com>
2025-06-26 06:39:40 +0000

    operation

---
+++ basics/opr.js
@@ -0,0 +1,31 @@
+let value = 3 
+let negValue = -value
+console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(3**2);
+console.log(2/2);
+console.log(3%2);*/
+ 
+let str1 = "hi"
+let str2 = "Ayush"
+console.log(str1+str2);
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+/* String first then all considered
+ into string otherwise whatever is first */
+
+ console.log(true+1);
+ console.log(+"");
+ 
+ let num1,num2,num3
+
+ num1=num2=num3= 2 + 2
+
+ 
